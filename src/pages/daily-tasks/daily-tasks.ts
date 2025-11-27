444e89f1bc8c51c554f3ea45c3f7aefd39b9c1c8
ItsLok0 <77623807+ItsLok0@users.noreply.github.com>
2025-11-27 13:17:43 +0100

    Ajout des composants + modifs css

---
+++ src/pages/daily-tasks/daily-tasks.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-tasks',
+  imports: [],
+  templateUrl: './daily-tasks.html',
+  styleUrl: './daily-tasks.scss',
+})
+export class DailyTasks {
+
+}

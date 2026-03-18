3e733c5cf09ffe9915b904dcb681191be334fbc0
ItsLok0 <77623807+ItsLok0@users.noreply.github.com>
2026-03-18 18:29:04 +0100

    fix: bad var env

--- set-env.js
+++ set-env.js
@@ -3,11 +3,11 @@ const envFile = `./src/environments/environment.ts`;
 let content = fs.readFileSync(envFile, 'utf8');
 
 // On remplace les placeholders par les vraies variables de Vercel
-content = content.replace('process.env.FIREBASE_API_KEY', process.env.FIREBASE_API_KEY);
-content = content.replace('process.env.FIREBASE_AUTH_DOMAIN', process.env.FIREBASE_AUTH_DOMAIN);
-content = content.replace('process.env.FIREBASE_PROJECT_ID', process.env.FIREBASE_PROJECT_ID);
-content = content.replace('process.env.FIREBASE_STORAGE_BUCKET', process.env.FIREBASE_STORAGE_BUCKET);
-content = content.replace('process.env.FIREBASE_MESSAGING_SENDER_ID', process.env.FIREBASE_MESSAGING_SENDER_ID);
-content = content.replace('process.env.FIREBASE_APP_ID', process.env.FIREBASE_APP_ID);
+content = content.replace('process.env.FIREBASE_API_KEY', process.env.NG_APP_FIREBASE_APP_ID);
+content = content.replace('process.env.FIREBASE_AUTH_DOMAIN', process.env.NG_APP_FIREBASE_AUTH_DOMAIN);
+content = content.replace('process.env.FIREBASE_PROJECT_ID', process.env.NG_APP_FIREBASE_PROJECT_ID);
+content = content.replace('process.env.FIREBASE_STORAGE_BUCKET', process.env.NG_APP_FIREBASE_STORAGE_BUCKET);
+content = content.replace('process.env.FIREBASE_MESSAGING_SENDER_ID', process.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID);
+content = content.replace('process.env.FIREBASE_APP_ID', process.env.NG_APP_FIREBASE_APP_ID);
 
 fs.writeFileSync(envFile, content);

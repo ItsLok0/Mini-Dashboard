8d0085ffad69a6e79c7fed6cea3e462cb281793e
ItsLok0 <77623807+ItsLok0@users.noreply.github.com>
2026-03-18 18:26:59 +0100

    fix: vercel env var

--- set-env.js
+++ set-env.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
-const targetPath = './src/environments/environment.prod.ts';
+const envFile = `./src/environments/environment.ts`;
+let content = fs.readFileSync(envFile, 'utf8');
 
-const envConfigFile = `
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: '${process.env.FIREBASE_API_KEY}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
-    appId: '${process.env.FIREBASE_APP_ID}'
-  }
-};
-`;
+// On remplace les placeholders par les vraies variables de Vercel
+content = content.replace('process.env.FIREBASE_API_KEY', process.env.FIREBASE_API_KEY);
+content = content.replace('process.env.FIREBASE_AUTH_DOMAIN', process.env.FIREBASE_AUTH_DOMAIN);
+content = content.replace('process.env.FIREBASE_PROJECT_ID', process.env.FIREBASE_PROJECT_ID);
+content = content.replace('process.env.FIREBASE_STORAGE_BUCKET', process.env.FIREBASE_STORAGE_BUCKET);
+content = content.replace('process.env.FIREBASE_MESSAGING_SENDER_ID', process.env.FIREBASE_MESSAGING_SENDER_ID);
+content = content.replace('process.env.FIREBASE_APP_ID', process.env.FIREBASE_APP_ID);
 
-fs.writeFileSync(targetPath, envConfigFile);
+fs.writeFileSync(envFile, content);

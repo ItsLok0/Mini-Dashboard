3eaea1e30555af73d1429248365b84cf214f1dab
ItsLok0 <77623807+ItsLok0@users.noreply.github.com>
2026-03-18 18:40:13 +0100

    fix: test set-env

--- set-env.js
+++ set-env.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
-const envFile = `./src/environments/environment.ts`;
-let content = fs.readFileSync(envFile, 'utf8');
+const targetPath = './src/environments/environment.prod.ts';
 
-// On remplace les placeholders par les vraies variables de Vercel
-content = content.replace('process.env.FIREBASE_API_KEY', process.env.NG_APP_FIREBASE_APP_ID);
-content = content.replace('process.env.FIREBASE_AUTH_DOMAIN', process.env.NG_APP_FIREBASE_AUTH_DOMAIN);
-content = content.replace('process.env.FIREBASE_PROJECT_ID', process.env.NG_APP_FIREBASE_PROJECT_ID);
-content = content.replace('process.env.FIREBASE_STORAGE_BUCKET', process.env.NG_APP_FIREBASE_STORAGE_BUCKET);
-content = content.replace('process.env.FIREBASE_MESSAGING_SENDER_ID', process.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID);
-content = content.replace('process.env.FIREBASE_APP_ID', process.env.NG_APP_FIREBASE_APP_ID);
+// On crée le contenu du fichier DIRECTEMENT avec les vraies valeurs
+const envConfigFile = `
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '${process.env.FIREBASE_API_KEY || ""}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN || ""}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID || ""}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET || ""}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID || ""}',
+    appId: '${process.env.FIREBASE_APP_ID || ""}'
+  }
+};
+`;
 
-fs.writeFileSync(envFile, content);
+console.log("Injecting environment variables...");
+fs.writeFileSync(targetPath, envConfigFile);
+console.log(`File generated at ${targetPath}`);

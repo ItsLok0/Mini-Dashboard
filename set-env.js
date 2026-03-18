eb5787751f3fe2dc4f91fdd36463b1677875b582
ItsLok0 <77623807+ItsLok0@users.noreply.github.com>
2026-03-18 18:44:00 +0100

    fix: set-env v2

--- set-env.js
+++ set-env.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
-const targetPath = './src/environments/environment.prod.ts';
+const path = require('path');
 
-// On crée le contenu du fichier DIRECTEMENT avec les vraies valeurs
-const envConfigFile = `
-export const environment = {
+const targetPath = path.join(__dirname, './src/environments/environment.prod.ts');
+
+const envConfigFile = `export const environment = {
   production: true,
   firebase: {
-    apiKey: '${process.env.FIREBASE_API_KEY || ""}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN || ""}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID || ""}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET || ""}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID || ""}',
-    appId: '${process.env.FIREBASE_APP_ID || ""}'
+    apiKey: '${process.env.NG_APP_FIREBASE_API_KEY || ""}',
+    authDomain: '${process.env.NG_APP_FIREBASE_AUTH_DOMAIN || ""}',
+    projectId: '${process.env.NG_APP_FIREBASE_PROJECT_ID || ""}',
+    storageBucket: '${process.env.NG_APP_FIREBASE_STORAGE_BUCKET || ""}',
+    messagingSenderId: '${process.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID || ""}',
+    appId: '${process.env.NG_APP_FIREBASE_APP_ID || ""}'
   }
 };
 `;
 
-console.log("Injecting environment variables...");
+console.log('--- Generation du fichier environment.prod.ts ---');
 fs.writeFileSync(targetPath, envConfigFile);
-console.log(`File generated at ${targetPath}`);
+console.log('--- Variables injectées avec succès ---');
